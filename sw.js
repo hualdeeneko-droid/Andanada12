618be3abb20eaee44d65bc8433a90b0cc9709b03
hualdeeneko-droid <hualdeeneko@gmail.com>
2026-06-29 07:34:04 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,3 @@
-self.addEventListener('fetch',()=>{});
+const CACHE='andanada12-v7';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','index.html','manifest.json','assets/emoticono.jpg']))));
+self.addEventListener('fetch',e=>{ if(e.request.url.includes('docs.google.com')) return; e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
